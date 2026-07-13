546d044a6b896f6ed15ad682bcfcc88dca3ac6e6
MuhaabDev <mohabjr111@gmail.com>
2026-07-13 08:47:07 +0300

    adding class accounts and refactoring

--- Stock Portfolio Tracker/main/Program.cs
+++ Stock Portfolio Tracker/main/Program.cs
@@ -4,6 +4,11 @@ namespace main
 {
     internal class Program
     {
+        
+        // todo : you need to find how to create a vector of a class
+        static user[] users = new user[10]; // to store the object for each user
+        static int noOfAccounts = 0;
+
         static void Main(string[] args)
         {
             Console.WriteLine(@"---------------------------------------------------------------------
@@ -15,63 +20,29 @@ namespace main
                 Console.WriteLine("1: Login \t\t 2: Register \t\t 3: Exit");
                 int op = int.Parse(Console.ReadLine());
 
-                // todo : you need to find how to create a vector of a class
-                user[] users = new user[10]; // to store the object for each user
-                int noOfAccounts = 0;
-
-                if (op == 1)
+                if (op == 1) // Login
                 {
-                    bool userNameFound = false, passwordFound = false;
                     Console.Write("Username: ");
                     string userName = Console.ReadLine();
-                    for(int i = 0; i < noOfAccounts; i++)
-                    {
-                        if(userName == users[i].username)
-                        {
-                            userNameFound = true;
-                        }
-                    }
                     Console.Write("Password: ");
                     string password = Console.ReadLine();
-                    for (int i = 0; i < noOfAccounts; i++)
-                    {
-                        if (password == users[i].password)
-                        {
-                            passwordFound = true;
-                        }
-                    }
 
-                    if(userNameFound && passwordFound)
+                    if(checkAccount(userName, password))
                     {
-                        Console.WriteLine($"You Have Loged In Successfuly , Welcome {userName}");
+                        Console.WriteLine("Login Succesfully");
                     }
                     else
                     {
-                        if (!userNameFound)
-                        {
-                            Console.WriteLine("This Username doesnt Exist , Try Again");
-                        }else
-                        {
-                            Console.WriteLine("Wrong Password");
-                        }
+                        Console.WriteLine("Login Failed");
                     }
                 }
                 else if (op == 2)
                 {
-                    // todo : you need to check if the username is not taken
                     Console.WriteLine("Enter Your Username : ");
                     string userName = Console.ReadLine();
-
-                    // todo : you need to check if the password is strong
                     Console.WriteLine("Enter Your Password : ");
                     string password = Console.ReadLine();
-
-                    users[noOfAccounts] = new user();
-                    users[noOfAccounts].username = userName;
-                    users[noOfAccounts].password = password;
-                    Console.WriteLine("Account Created Successfully");
-                    noOfAccounts++;
-                    
+                    RegisterAccount(userName, password);        
                 }
                 else if (op == 3)
                 {
@@ -83,5 +54,31 @@ namespace main
                 }
             }
         }
+
+        static bool checkAccount(string username, string password) {
+            bool userNameFound = false, passwordFound = false;
+           
+            for (int i = 0; i < noOfAccounts; i++)
+            {
+                if (username == users[i].username) userNameFound = true;
+            }
+
+            for (int i = 0; i < noOfAccounts; i++)
+            {
+                if (password == users[i].password) passwordFound = true;
+            }
+            return (userNameFound &&  passwordFound);
+        }
+
+        static void RegisterAccount(string username, string password) {
+            // todo : you need to check if the password is strong
+            // todo : you need to check if the username is not taken
+            users[noOfAccounts] = new user();
+            users[noOfAccounts].username = username;
+            users[noOfAccounts].password = password;
+            Console.WriteLine("Account Created Successfully");
+            noOfAccounts++;
+        }
+       
     }
 }
